41c21ebf75099ce54aff1c2822ccbc90cf07a5d8
Nestor Castillo <nestor.castillo@izea.com>
2014-11-19 21:43:37 -0500

    dojo progress

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -1,3 +1,21 @@
 class RPNCalculator
-
+  def evaluate(input)
+    stack = []
+    token = input.split
+    token.each do |t|
+      if t == "+"
+        a = stack.pop(2)
+        stack << a[0] + a[1]
+      elsif t == "*"
+        a = stack.pop(2)
+        stack << a[0] * a[1]
+      elsif t == "-"
+        a = stack.pop(2)
+        stack << a[0] - a[1]
+      elsif t.to_i.is_a? Integer
+        stack << t.to_i
+      end
+    end
+    stack.first
+  end
 end
--- spec/rpn_calculator_spec.rb
+++ spec/rpn_calculator_spec.rb
@@ -1,5 +1,36 @@
 require './lib/rpn_calculator'
 
 describe RPNCalculator do
+  let(:calculator) { RPNCalculator.new }
+  it "should expect '-1' to return -1" do
+    expect(calculator.evaluate("-1")).to eq -1
+  end
 
+  it "should expect '1 1 +' to return 2" do
+    expect(calculator.evaluate("1 1 +")).to eq 2
+  end
+
+  it "should expect '1 -1 +' to return 0" do
+    expect(calculator.evaluate("1 -1 +")).to eq 0
+  end
+
+  it "should expect '2 2 *' to return 4" do
+    expect(calculator.evaluate("2 2 *")).to eq 4
+  end
+
+  it "should expect '5 10 -' to return -5" do
+    expect(calculator.evaluate("5 10 -")).to eq 5 - 10
+  end
+
+  it "should expect '1 2 3 4 + + +' to return 1 + 2 + 3 + 4" do
+    expect(calculator.evaluate("1 2 3 4 + + +")).to eq 1 + 2 + 3 + 4
+  end
+
+  it "should expect '1 2 + 3 4 + *' to return (2 + 1) * (4 + 3)" do
+    expect(calculator.evaluate("1 2 + 3 4 + *")).to eq (2 + 1) * (4 + 3)
+  end
+
+  it "should expect '20 10 5 4 + * -' to return 20 - 10 * (5 + 4)" do
+    expect(calculator.evaluate("20 10 5 4 + * -")).to eq 20 - 10 * (5 + 4)
+  end
 end
